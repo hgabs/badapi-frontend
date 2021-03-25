da257e1b1728dc8cb3afa6facf1ebb491ac1d424
hgabs <14847474+hgabs@users.noreply.github.com>
2021-03-25 16:46:07 +0200

    Make page-not-found text responsive in size

--- src/index.js
+++ src/index.js
@@ -2,11 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './App.css';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 
+
+const theme = createMuiTheme();
+
+theme.typography.h2 = {
+  ...theme.typography.h2,
+  [theme.breakpoints.down('xs')]: {
+    fontSize: '2rem',
+  },
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
